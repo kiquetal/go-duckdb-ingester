76a1823af9608e9abd27ddb906a860b940d9065f
kiquetal <kiquetal@gmail.com>
2025-04-08 23:50:50 -0300

    Adding

---
+++ call-sum-per-day.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PROMETHEUS_URL="http://localhost:9080"
+START_TIME="2025-04-07T00:00:00Z"
+END_TIME="2025-04-07T23:59:59Z"
+APP_NAME="memento"
+
+# Query to get the total number of requests for the day
+QUERY="sum(increase(istio_requests_total{app=\"${APP_NAME}\"}[24h])) by (app)"
+
+# To get the value at the end of the day
+END_TIME_UNIX=$(date -d "$END_TIME" +%s)
+
+curl -g "${PROMETHEUS_URL}/api/v1/query" \
+  --data-urlencode "query=$QUERY" \
+  --data-urlencode "time=$END_TIME_UNIX"
+
